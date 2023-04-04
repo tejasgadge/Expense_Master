916d18afd43b8eb37ce8a1b94c5bae392768ca23
tejasgadge <tejasgadge2882@gmail.com>
2023-04-04 10:10:49 +0530

    Updates

---
+++ precache-manifest.ee62d09338f2af24a7d55f1bfe78dc51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5db3b99664c06b0e1a3c",
+    "url": "/Expense_Master/static/css/main.55ff3c8f.chunk.css"
+  },
+  {
+    "revision": "5db3b99664c06b0e1a3c",
+    "url": "/Expense_Master/static/js/main.5db3b996.chunk.js"
+  },
+  {
+    "revision": "22f3400ad5bc08de86a0",
+    "url": "/Expense_Master/static/js/1.22f3400a.chunk.js"
+  },
+  {
+    "revision": "0111c43b44cc49c72cad",
+    "url": "/Expense_Master/static/js/runtime~main.0111c43b.js"
+  },
+  {
+    "revision": "cdeb8a956e9db935aa192b190cde7adc",
+    "url": "/Expense_Master/index.html"
+  }
+];
